ba819e802cdf34f360320ddd33d7ca11b57da4d3
Rcyo <ringo_content@hotmail.com>
2020-08-14 05:14:25 +0200

    Updated aria-label=""
    Since Tinder did a update on their web version "Like" isnt used anymore. The value is now "Fun".

--- like.js
+++ like.js
@@ -2,7 +2,7 @@ var confirmBox = confirm('Start swiping right?');
 var count = 1;
 var like = function(){
     if (confirmBox){
-      document.querySelector('[aria-label="Like"]').click();
+      document.querySelector('[aria-label="Fun"]').click();
       console.log('Liked ' + count);						  count++;												};
     setTimeout(like, 100);
 };
